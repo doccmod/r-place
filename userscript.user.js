67fa0684e2751682cb6f428fb2e631f92e574c6a
doccmod <57057971+doccmod@users.noreply.github.com>
2022-04-02 09:46:43 -0400

    Create userscript.user.js

---
+++ userscript.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         template
+// @namespace    http://tampermonkey.net/
+// @version      1.0 plain
+// @description  Destiny r/place template
+// @author       docmod
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/Anticept/r-place/template.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
